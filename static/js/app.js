74134fb94b472bc8f597a3579fd63c1746656ed9
smithc6136 <smithc6136@yahoo.com>
2020-09-24 20:34:34 -0400

    structuring init function

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,51 @@
 // reference: http://learnjsdata.com/read_data.html
 d3.json("../samples.json").then(function(data) {
     console.log(data);
-  });
+    data.names.forEach(function (name) {
+        console.log(name);
+    });
+});
+
+// Initialize the page with a dropdown menu
+var option = document.createElement("option");
+option.text = "Text";
+option.value = "myvalue";
+var select = document.getElementById("daySelect");
+select.append(option);
+
+// Initialize the page with a default plot
+function init() {
+    d3.select("#selDataset")
+    d3.json("../samples.json").then(function(data) {
+        console.log(data);
+        data.names.forEach(function (name) {
+            console.log(name);
+        });
+    });
+    // build the initial plots with the first sample by calling both functions you define
+    var firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  }
+
+
+// Two main asks:
+  // Initialize
+    // hydrate (populate) the dropdown menu
+        // <option value="dog">Dog</option>
+          // var option = document.createElement("option");
+          // option.text = "Text";
+          // option.value = "myvalue";
+          // var select = document.getElementById("daySelect");
+          // select.appendChild(option);
+        // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
+        // for loop to iterate through all "names" (data.names) to populate select options
+    // Filter samples.json to get data associated with default
+        // Render the bar chart with data from the selected name (default value)
+        // Render bubble chart
+        // render metadata (key value pair)
+  // Respond to user input
+      // rerender bar chart, bubble chart, and metadata based on user selection
 
 
 // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
@@ -23,25 +67,25 @@ d3.json("../samples.json").then(function(data) {
 // var Frequency = ??
 // var OTU = ??
 
-// Create the Trace
-var trace1 = {
-    x: Frequency,
-    y: OTU,
-    type: "bar"
-  };
+// // Create the Trace
+// var trace1 = {
+//     x: Frequency,
+//     y: OTU,
+//     type: "bar"
+//   };
   
-  // Create the data array for the plot
-  var data = [trace1];
+//   // Create the data array for the plot
+//   var data = [trace1];
   
-  // Define the plot layout
-  var layout = {
-    title: "Top Ten OTUs per Individual",
-    xaxis: { title: "Frequency" },
-    yaxis: { title: "OTU" }
-  };
+//   // Define the plot layout
+//   var layout = {
+//     title: "Top Ten OTUs per Individual",
+//     xaxis: { title: "Frequency" },
+//     yaxis: { title: "OTU" }
+//   };
   
-  // Plot the chart to a div tag with id "bar-plot"
-  Plotly.newPlot("bar-plot", data, layout);
+//   // Plot the chart to a div tag with id "bar-plot"
+//   Plotly.newPlot("bar-plot", data, layout);
   
 
 // 3. Create a bubble chart that displays each sample.
@@ -50,10 +94,11 @@ var trace1 = {
   // Use sample_values for the marker size
   // Use otu_ids for the marker colors
   // Use otu_labels for the text values.
+  // DOCUMENTATION: https://plotly.com/javascript/bubble-charts/
+  
+// 4. (WHAT)Display the sample metadata, i.e., an individual's demographic information.
 
-// 4. Display the sample metadata, i.e., an individual's demographic information.
-
-// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
+// 5. (HOW to do 4) Display each key-value pair from the metadata JSON object somewhere on the page.
   
 // 6. Update all of the plots any time that a new sample is selected.
   // 15.2 Activity 9
