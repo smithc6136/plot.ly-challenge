62e2b0ce2cc80fb014256c0bdb6fb3b63e311983
smithc6136 <smithc6136@yahoo.com>
2020-10-05 19:47:35 -0400

    edited code for bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -9,34 +9,9 @@
       // rerender bar chart (15.2 Activity 8), bubble chart, and metadata based on user selection
 
 
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-    // Top 10: if (bar_ids.length <= 10) {
-        // loop through OTU Data: for ...
-        // Append data: bar.push(values)
-        // }
-    // Use sample_values as the values for the bar chart
-    // Use otu_ids as the labels for the bar chart
-    // Use otu_labels as the hovertext for the chart.
-
 
 // 1. Use the D3 library to read in samples.json.
 // reference: http://learnjsdata.com/read_data.html
-// d3.json("../samples.json").then(function (data) {
-//   console.log(data);
-//   data.names.forEach(function (name) {
-//       console.log(name);
-//   });
-//   // populate the dropdown menu
-//   var sampleNames = data.names;
-//   sampleNames.forEach((name) => {
-//       selector
-//           .append("option")
-//           .text(name)
-//           .property("value", name);
-//   });
-// });
-
-// Initialize the page with a default plot
 function init() {
     var selector = d3.select("#selDataset");
     d3.json("../samples.json").then(function(data) {
@@ -59,6 +34,11 @@ function init() {
     });
   }
 
+
+// 2. Create a horizontal bar chart with top 10 OTUs for the individual.
+    // Use sample_values as the values for the bar chart
+    // Use otu_ids as the labels for the bar chart
+    // Use otu_labels as the hovertext for the chart.
 function buildCharts(id) {
     d3.json("../samples.json").then(function(data) {
         // this returns array of objects
@@ -79,8 +59,6 @@ function barChart(sample) {
     console.log("labels", labels);
     var hoverText = sample.otu_labels.slice(0,10);
     console.log("hover text", hoverText);
-// // // // Reverse the array to accommodate Plotly's defaults - IS THIS NECESSARY?
-// // // reversedData = slicedData.reverse();
 
     // Trace1 
     var trace1 = [{
@@ -104,29 +82,6 @@ function barChart(sample) {
 }
 
 
-
-// // data
-// var data = [trace1];
-
-
-// // Define x and y axis but the info is in the json file we aren't typing it??
-// // var Frequency = ??
-// // var OTU = ??
-
-// // // Create the Trace
-// // var trace1 = {
-// //     x: Frequency,
-// //     y: OTU,
-// //     type: "bar"
-// //   };
-  
-// //   // Create the data array for the plot
-// //   var data = [trace1];
-
-// //   // Plot the chart to a div tag with id "bar-plot"
-// //   Plotly.newPlot("bar-plot", data, layout);
-  
-
 // // 3. Create a bubble chart that displays each sample.
 //   // Use otu_ids for the x values
 //   // Use sample_values for the y values
@@ -135,32 +90,47 @@ function barChart(sample) {
 //   // Use otu_labels for the text values.
 //   // DOCUMENTATION: https://plotly.com/javascript/bubble-charts/
 
-//   var trace1 = {
-//     x: [1, 2, 3, 4],
-//     y: [10, 11, 12, 13],
-//     mode: 'markers',
-//     marker: {
-//       size: [40, 60, 80, 100]
+// function bubbleChart(sample) {
+//     var trace1 = {
+//         x: sample.otu_ids,
+//         y: sample_values,
+//         mode: 'markers',
+//         marker: {
+//         size: sample_values
+//         }
 //     }
-//   };
+// };
   
-//   var data = [trace1];
+//     var data = [trace1];
   
-//   var layout = {
-//     title: 'Marker Size',
-//     showlegend: false,
-//     height: 600,
-//     width: 600
-//   };
+//     var layout = {
+//         title: 'Marker Size',
+//         showlegend: false,
+//         height: 600,
+//         width: 600
+//     };
   
 //   Plotly.newPlot('myDiv', data, layout);
   
-// // 4. (WHAT)Display the sample metadata, i.e., an individual's demographic information.
+// 4. (WHAT)Display the sample metadata, i.e., an individual's demographic information.
+    // Div in HTML: <div id="sample-metadata" class="panel-body"></div> 
 
-// // 5. (HOW to do 4) Display each key-value pair from the metadata JSON object somewhere on the page.
-  
-// // 6. Update all of the plots any time that a new sample is selected.
-//   // 15.2 Activity 9
+// 5. (HOW to do 4) Display each key-value pair from the metadata JSON object somewhere on the page.
+    // function metadata(sample) {
+    //     metadata=d3.keys(metadata);
+    // }
+
+function metadata() {
+    var selector = d3.select("#sample-metadata");
+    d3.json("../samples.json").then(function(data) {
+        console.log(data);
+        data.names.forEach(function (name) {
+            console.log(name);
+        });
+    });
+}
+// 6. Update all of the plots any time that a new sample is selected.
+   // 15.2 Activity 9
 
 
 init();
